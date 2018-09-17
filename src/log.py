5bf2b54800d39c06f960afeb21b226225a7339f8
CodeWitch <codewitch@Code-Witch-Mac.local>
2018-09-17 14:51:30 -0600

    some refactoring

---
+++ src/log.py
@@ -0,0 +1,16 @@
+
+
+class Logger:
+    def __init__(self):
+        self.buffer = []
+        self.log = 'log.bot'
+    
+    
+    def queue_data(self, data):
+        pass
+
+    def write(self):
+        pass
+
+    def read(self):
+        pass
